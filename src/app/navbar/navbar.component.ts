0b9d84e55083786ee12fb89b22315e14f3eecbae
Rajkumar Kanase <rajkanase11@gmail.com>
2017-09-01 18:32:58 +0530

    sidebar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -12,4 +12,10 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
   }
 
+  onClick(){
+    console.log("innnn");
+    
+    document.getElementById("sidebar").classList.toggle("active");
+  }
+
 }
